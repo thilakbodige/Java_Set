655c20768a325bbd599de869c36e2e87a5a45339
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-04-14 10:58:35 +0530

    Add files via upload

---
+++ Tree_Set.java
@@ -0,0 +1,17 @@
+package Set;
+import java.util.*;
+public class Tree_Set
+{
+	public static void main(String[] args)
+	{
+		TreeSet t=new TreeSet();
+		t.add("sai");
+		t.add("tilak");
+		t.add("Spandana");
+		t.add("akhila");
+		t.add("spandanas");
+		t.add("spandanas");
+		System.out.println(t);
+		}
+
+}
